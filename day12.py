75da5221c3c2c8f40205089533d8e4d8660927c8
Brian Stewart <brianstewarteditor@gmail.com>
2023-12-21 17:21:04 +1000

    rework day 12

--- day12.py
+++ day12.py
@@ -1,5 +1,4 @@
 import multiprocessing
-from collections import deque 
 
 def check_if_satisfies_text(combo, required):
     springs = combo.split(".")
@@ -13,22 +12,6 @@ def check_if_satisfies_text(combo, required):
             return False
         i += 1
     if i != len(required):
-        return False
-    return True
-
-def check_if_satisfies_list(combo, required, partial=False):
-    i = 0
-    for j, spring in enumerate(combo):
-        if i >= len(required):
-            return False
-        if spring == 0:
-            continue
-        if partial and spring > required[i]:
-            return False
-        elif (not partial or j < len(combo) - 1) and spring != required[i]:
-            return False
-        i += 1
-    if not partial and i != len(required):
         return False
     return True
 
@@ -56,13 +39,12 @@ def solve1(input_data):
         for combo in combos:
             if check_if_satisfies_text(combo, required):
                 st += 1
-                # print(combo, required)
                 
         t += st
 
     return t
 
-def solve_2_lines(inputs):
+def solve_single_line(inputs):
     input_data = inputs[0]
     j = inputs[1]
     data = input_data.split("\n")
@@ -74,156 +56,62 @@ def solve_2_lines(inputs):
     springs = springs + "?" + springs + "?" + springs + "?" + springs + "?" + springs
     required = required + required + required + required + required
 
-    combos = deque()
-    combos.append((0,0,0))
-    st = 0
+    # Dictionary format (string_index,required_index,current_run) = max_combos
+    combos_at_index = {}
 
-    while len(combos) > 0:
-        (current_amount, spring_index, required_index) = combos.pop()
+    # Initialise end values
+    for n in range(max(required)+1):
+        for k in range(len(required)+1):
+            combos_at_index[(len(springs),k,n)] = 0
+    combos_at_index[(len(springs),len(required),0)] = 1
+    combos_at_index[(len(springs),len(required)-1,required[-1])] = 1
 
-        if spring_index == len(springs):
-            if required_index == len(required) and current_amount == 0:
-                st += 1
-            elif required_index == len(required) - 1 and current_amount == required[required_index]:
-                st += 1
-            continue
+    # Loop through keys backwards and populate, i is spring index, k is required index, n is current run
+    for i in range(len(springs)-1,-1,-1):
+        for k in range(len(required),-1,-1):
+            # if we reached the end of the required numbers then only deal with n = 0
+            if k >= len(required):
+                st = 0
+                if springs[i] == "?" or springs[i] == ".":
+                    combos_at_index[(i,k,0)] = combos_at_index[(i+1,k,0)]
+                else:
+                    combos_at_index[(i,k,0)] = 0
+                continue
+            for n in range(required[k]+1):
+                assert (i,k,n) not in combos_at_index
 
-        if required_index == len(required) and current_amount == 0 and springs[spring_index] != "#":
-            combos.append((current_amount, spring_index + 1, required_index))
-            continue
+                st = 0
+                if i + 1 <= len(springs):
+                    # found a new spring
+                    if (springs[i] == "?" or springs[i] == "#") and k < len(required):
+                        if n + 1 <= required[k]:
+                            st += combos_at_index[(i+1,k,n+1)]
+                    # found an empty square
+                    if springs[i] == "?" or springs[i] == ".":
+                        if n == 0:
+                            st += combos_at_index[(i+1,k,n)]
+                        elif n == required[k]:
+                            if k + 1 <= len(required):
+                                st += combos_at_index[(i+1,k+1,0)]
+                combos_at_index[(i,k,n)] = st
 
-        if springs[spring_index] == "?":
-            # this is a new spring
-            if current_amount + 1 <= required[required_index]:
-                combos.append((current_amount + 1, spring_index + 1, required_index))
-            # was coming from a empty and this is an empty
-            if current_amount == 0:
-                combos.append((current_amount, spring_index + 1, required_index))
-            # was coming from a spring and this is an empty 
-            if current_amount == required[required_index]:
-                combos.append((0, spring_index + 1, required_index + 1))
-        
-        elif springs[spring_index] == "#":
-            if current_amount + 1 <= required[required_index]:
-                combos.append((current_amount + 1, spring_index + 1, required_index))
-        else:
-            # was coming from a empty
-            if current_amount == 0:
-                combos.append((current_amount, spring_index + 1, required_index))
-            # was coming from a spring
-            if current_amount == required[required_index]:
-                combos.append((0, spring_index + 1, required_index + 1))
-        # print(combos)
-
-    print(j, st)
-    return st
+    return combos_at_index[(0,0,0)]
 
 def solve2(input_data):
     global input_value
     input_value = input_data
     data = input_data.split("\n")
-    # solve_2_lines((input_data, 1))
+
     cpu_count = multiprocessing.cpu_count()
     input_params_list = [(input_data, i) for i in range(len(data))]
     t = 0
 
     pool = multiprocessing.Pool(cpu_count)
-    results = pool.map(solve_2_lines, input_params_list)
+    results = pool.map(solve_single_line, input_params_list)
     
     pool.close()
     pool.join()
-    print(sum(results))
-
-
-
-    # for i in range(len(data)):
-    #     threads.append(Process(target=solve_2_lines, args=(input_data,i,ans)))
-    
-    # for thread in threads:
-    #     thread.start()
-
-    # for thread in threads:
-    #     thread.join()
-
-    # a = ans.get()
-    # t += a
-    # while a > 0:
-    #     a = ans.get()
-    #     print("TOTAL:", t)
-    #     t += a
-    
-
-
-
-    # for line in data:
-    #     springs, required_raw = line.split(" ")
-    #     required = [int(v) for v in required_raw.split(",")]
-
-    #     springs = springs + "?" + springs + "?" + springs + "?" + springs + "?" + springs
-    #     required = required + required + required + required + required
-    #     springs_initial = springs.count("#")
-    #     required_count = sum(required)
-    #     max_allowed = required_count - springs_initial
-    #     spring_length = len(springs)
-    #     combos = [([0],False)]
-    #     st = 0
-
-    #     for i in range(spring_length):
-    #         new_combos = []
-
-    #         # print(i)
-    #         while len(combos) > 0:
-    #             (cur_combo, on_run) = combos.pop()
-    #             springs_current = sum(cur_combo)
-    #             springs_upcoming = springs[i:].count("#")
-    #             springs_total_so_far = springs_current + springs_upcoming
-    #             # print(springs_current)
-    #             if springs[i] == "?":
-    #                 if on_run:
-    #                     temp_combo_add = cur_combo.copy()
-    #                     temp_combo_add[-1] = temp_combo_add[-1]+1
-    #                     if (springs_total_so_far < required_count 
-    #                         and check_if_satisfies_list(temp_combo_add, required, True)):
-    #                         new_combos.append((temp_combo_add, True))
-    #                     temp_combo_same = cur_combo.copy()
-    #                     new_combos.append((temp_combo_same, False))
-                        
-    #                 else:
-    #                     temp_combo_add2 = cur_combo.copy()
-    #                     temp_combo_add2.append(1)
-    #                     if (springs_total_so_far < required_count 
-    #                         and check_if_satisfies_list(temp_combo_add2, required, True)):
-    #                         new_combos.append((temp_combo_add2, True))
-    #                     temp_combo_same2 = cur_combo.copy()
-    #                     new_combos.append((temp_combo_same2, False))
-    #             elif springs[i] == "#":
-    #                 temp_combo_add7 = cur_combo.copy()
-    #                 if on_run:
-    #                     temp_combo_add7[-1] = temp_combo_add7[-1]+1
-    #                     if (springs_total_so_far <= required_count
-    #                         and check_if_satisfies_list(temp_combo_add7, required, True)):
-    #                         new_combos.append((temp_combo_add7, True))
-
-    #                 else:
-    #                     temp_combo_add7.append(1)
-    #                     if check_if_satisfies_list(temp_combo_add7, required, True):
-    #                         new_combos.append((temp_combo_add7, True))
-    #             else:
-    #                 temp_combo_same4 = cur_combo.copy()
-    #                 new_combos.append((temp_combo_same4, False))
-    #             # print(new_combos)
-    #         combos = new_combos
-
-    #     # print(len(combos))
-    #     for combo, run in combos:
-    #         if check_if_satisfies_list(combo, required):
-    #             st += 1
-                
-    #             # print(combo, required)
-    #     print(st)     
-    #     t += st
-
-    # return t
+    return sum(results)
 
 
 if __name__ == "__main__":
